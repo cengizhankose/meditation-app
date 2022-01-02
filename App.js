b652fa6596fb295f1172a0361d4f5235f2f70c29
Cengizhan KOSE <kose651@gmail.com>
2022-01-03 01:20:09 +0300

    firebase configuration

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgkag3WWCA_S4X8iF-ZNgjhVKZv-cDK1M",
+  authDomain: "meditation-app-9925c.firebaseapp.com",
+  projectId: "meditation-app-9925c",
+  storageBucket: "meditation-app-9925c.appspot.com",
+  messagingSenderId: "128123729819",
+  appId: "1:128123729819:web:4c8d0c193bf03b8a23933e",
+  measurementId: "G-HCWV8F5W8P",
+};
 
 export default function App() {
+  const app = initializeApp(firebaseConfig);
   return (
     <View style={styles.container}>
       <Text>Meditation App</Text>
