8d109065cd7be2e5dd7a9cfee25752d693cf9a84
Cengizhan KOSE <kose651@gmail.com>
2022-01-04 05:25:50 +0300

    Update SoundsScreen.js

--- src/pages/BottomTabScreens/SoundsScreen.js
+++ src/pages/BottomTabScreens/SoundsScreen.js
@@ -1,17 +1,70 @@
 import React from "react";
 import { View, Text } from "react-native";
+import SoundCard from "../../components/SoundCard";
+import CustomStatusBar from "../../components/CustomStatusBar";
+import SoundListItem from "../../components/SoundListItem";
+import { clearWarnings } from "react-native/Libraries/LogBox/Data/LogBoxData";
 
-function SoundsScreen() {
+function SoundsScreen({ navigation }) {
+  const soundCategories = [
+    {
+      icon: require("../../assets/images/sound-ic-1.png"),
+      title: "Painting Forest",
+      listeningCount: "59899",
+      duration: "20",
+    },
+    {
+      icon: require("../../assets/images/sound-ic-2.png"),
+      title: "Mountaineers",
+      listeningCount: "45697",
+      duration: "15",
+    },
+    {
+      icon: require("../../assets/images/sound-ic-3.png"),
+      title: "Lovely Deserts",
+      listeningCount: "9428",
+      duration: "39",
+    },
+    {
+      icon: require("../../assets/images/sound-ic-4.png"),
+      title: "The Hill Sides",
+      listeningCount: "52599",
+      duration: "50",
+    },
+  ];
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#253334",
       }}
     >
-      <Text>Sounds!</Text>
+      <CustomStatusBar />
+      <SoundCard
+        style={{
+          marginBottom: 20,
+          marginTop: 34,
+        }}
+        title={"Relax Sounds"}
+        subtitle={"Sometimes the most productive thing you can do is relax."}
+        index={0}
+      />
+      {soundCategories.map((sound, index) => {
+        return (
+          <SoundListItem
+            key={index}
+            icon={sound.icon}
+            title={sound.title}
+            listeningCount={sound.listeningCount}
+            duration={sound.duration}
+            onPress={(index) => {
+              navigation.navigate("NowPlaying", { data: sound });
+            }}
+          />
+        );
+      })}
     </View>
   );
 }
