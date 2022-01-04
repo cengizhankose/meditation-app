af61ba503314d02c2935b42bdaef19f1267de756
Cengizhan KOSE <kose651@gmail.com>
2022-01-04 05:25:47 +0300

    Update WatchButton.js

--- src/components/WatchButton.js
+++ src/components/WatchButton.js
@@ -2,45 +2,46 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
-const WatchButton = () => {
+const WatchButton = ({ isPlay }) => {
+  const styles = StyleSheet.create({
+    buttonRow: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    buttonContainer: {
+      width: 138,
+      height: 39,
+      borderRadius: 10,
+      backgroundColor: isPlay ? "white" : "#253334",
+      flexDirection: "column",
+      justifyContent: "center",
+      marginTop: 16,
+    },
+    watchTitle: {
+      fontFamily: "Alegreya_500Medium",
+      fontWeight: "500",
+      fontStyle: "normal",
+      fontSize: 15,
+      lineHeight: 18,
+      color: isPlay ? "black" : "white",
+    },
+  });
   return (
     <View style={styles.buttonContainer}>
       <View style={styles.buttonRow}>
-        <Text style={styles.watchTitle}>Watch Now</Text>
+        <Text style={styles.watchTitle}>
+          {isPlay ? "play now" : "Watch Now"}
+        </Text>
         <AntDesign
           style={{ marginLeft: 8 }}
           name="play"
           size={13}
-          color="white"
+          color={isPlay ? "black" : "white"}
         />
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonContainer: {
-    width: 138,
-    height: 39,
-    borderRadius: 10,
-    backgroundColor: "#253334",
-    flexDirection: "column",
-    justifyContent: "center",
-    marginTop: 16,
-  },
-  watchTitle: {
-    fontFamily: "Alegreya_500Medium",
-    fontWeight: "500",
-    fontStyle: "normal",
-    fontSize: 15,
-    lineHeight: 18,
-    color: "white",
-  },
-});
 
 export default WatchButton;
